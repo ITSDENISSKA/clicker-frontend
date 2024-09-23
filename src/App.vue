e93713880be05f0fd07572825d6cefbde60020c6
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-09-23 09:57:50 +0300

    update rating

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 <template>
   <div
-    class="gradient-background h-full flex flex-col justify-between bg-white overflow-hidden"
+    class="gradient-background h-screen flex flex-col justify-between bg-white overflow-hidden"
   >
     <RouterView class="h-full"></RouterView>
-    <div class="bottom-0 absolute w-full space-x-4 pb-6 pt-20 px-10 flex justify-between">
+    <div class="bottom-0 absolute w-full space-x-4 pb-6  pt-20 px-10 flex justify-between">
       <RouterLink to="/" class=" cursor-pointer px-4 ">
         <i class="pi pi-bitcoin !text-4xl hover:text-stone-400"></i>
       </RouterLink>
